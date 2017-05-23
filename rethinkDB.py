beacf2e2cd605cdc0b8d944bd7f2837dfebea341
nerotix <vierkantje80@gmail.com>
2017-05-23 16:35:48 +0200

    RethinkDB

--- rethinkDB.py
+++ rethinkDB.py
@@ -7,4 +7,10 @@ Webpanel: 146.185.180.205:8080
 import rethinkdb as r
 r.connect('146.185.180.205', 28015).repl()
 
-r.table('tv_shows').insert({ 'name': 'bassie en adriaan' }).run()
+#Table aanmaken in ISEN DB
+#r.db("ISEN").table_create("Sensoren").run()
+
+#Data toevoegen aan data
+r.db("ISEN").table('Sensoren').filter({"id": "1"})
+
+#r.db("ISEN").table("Sensoren").insert([{ "id": "1", "sensorID": "1", "Vochtigheid": "99"}]).run()
