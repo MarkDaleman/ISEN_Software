661280ff9a91f7834afe7ad27763bf4c470de9c1
Mark <markdaleman@icloud.com>
2017-06-06 11:49:55 +0200

    Sensor + Zenden werkend

--- Arduino Nano (Zenders)/zender/zender.ino
+++ Arduino Nano (Zenders)/zender/zender.ino
@@ -19,6 +19,12 @@
 int SensorNummer;
 int SensorWaarde;
 char Istr[6];
+/*
+ * Dit is nodig voor de soil meter, deze is op pin A0 aangesloten (Analoog)
+ * Ook moet de output value een integer zijn.
+ */
+int sensor_pin = A0; 
+int output_value ;
 
 /* 
  *  In de setup zetten wij neer wat de Arduino nodig heeft om te kunnen werken
@@ -30,29 +36,34 @@ char Istr[6];
 void setup()
 {
   Serial.begin(9600);    // Debugging only
+  Serial.println("VirtualWire setup...");
   vw_set_tx_pin(12);        
   // Initialise the IO and ISR
   vw_set_ptt_inverted(true); 
-  vw_setup(4000);  ec
+  vw_setup(4000); 
+  delay(250); 
+  Serial.println("VirtualWire setup complete");
   delay(250); 
 }
 /*
  * Hieronder is de Main loop. Deze zal continu draaien op de Arduino
  * Sensornummer is voor elke sensor anders. Deze Arduino heeft ID 5
- * De Waarde van de Sensor staat hier nu random tussen de 10 en 90 (om te testen)
+ * De Waarde van de komt uit de soil vochtigheid meter
  * Vervolgens stoppen we de waarde en sensornummer in de Array
  * De Array versturen we met vw_send richting de Raspberry Pi
  * Daarna wachten we tot het bericht is verzonden en gaan we in slaap
  */
-void loop()
-{      
-  int SensorNummer = 5;        
-  int SensorWaarde = random(10, 90);   
+void loop(){
+  Serial.println("Reading sensor and sending data");
+  output_value = analogRead(sensor_pin);
+  output_value = map(output_value,1023,140,0,100);
+  Serial.println(output_value);
+  int SensorNummer = 1;           
   Istr[0] = SensorNummer;
-  Istr[1] =  SensorWaarde;
-  if(SensorNummer > 0 and SensorWaarde > 10 and SensorWaarde < 100){
+  Istr[1] =  output_value;
+  if(SensorNummer > 0 and output_value < 100){
      vw_send((uint8_t *)Istr, strlen(Istr));
      vw_wait_tx(); // Wait until the whole message is gone
   }
-  delay(3000);
+  delay(1000);
 }
