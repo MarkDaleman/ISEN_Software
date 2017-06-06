e141dd08f127a823654ee5925b7e0da796d0837a
nerotix <vierkantje80@gmail.com>
2017-06-06 11:50:26 +0200

    serial update voor meer sensoren

--- Python/serialcommunication.py
+++ Python/serialcommunication.py
@@ -16,24 +16,45 @@ import sys
 
 output = " "
 ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
-dic = {}
 
-list = []
-dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0}
+dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0}
 varx = ""
 while True:
-<<<<<<< HEAD
 	#print(ser.readline())
-
 	varx = ser.readline().decode('utf-8').replace(" ", "")
-=======
 	varx = ser.readline().decode('utf-8').replace(":", "")
->>>>>>> 1b5825d7c8d5ce7d6df2fdae85944395b4cd8dcf
 	if varx == "":
 		continue
 	else:
-		if varx[0] == '5':
+		if varx[0] == '1':
+			dict[1] = varx[1] + varx[2]
+			print(dict[1])
+		elif varx[0] == '2':
+			dict[2] = varx[1] + varx[2]
+			print(dict[2])
+		elif varx[0] == '3':
+			dict[3] = varx[1] + varx[2]
+			print(dict[3])
+		elif varx[0] == '4':
+			dict[4] = varx[1] + varx[2]
+			print(dict[4])
+		elif varx[0] == '5':
 			dict[5] = varx[1] + varx[2]
 			print(dict[5])
+		elif varx[0] == '6':
+			dict[6] = varx[1] + varx[2]
+			print(dict[6])
+		elif varx[0] == '7':
+			dict[7] = varx[1] + varx[2]
+			print(dict[7])
+		elif varx[0] == '8':
+			dict[8] = varx[1] + varx[2]
+			print(dict[8])
+		elif varx[0] == '9':
+			dict[9] = varx[1] + varx[2]
+			print(dict[9])
+		elif varx[0] == '10':
+			dict[10] = varx[1] + varx[2]
+			print(dict[10])
 		else:
 			continue
