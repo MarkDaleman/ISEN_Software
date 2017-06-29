396ed2f12d784feda7cc6189830a435352fabef3
nerotix <vierkantje80@gmail.com>
2017-06-29 16:00:09 +0200

    script voor android notificaties

--- Python/push_notificatie.py
+++ Python/push_notificatie.py
@@ -2,29 +2,51 @@ from urllib2 import *
 import urllib
 import json
 import sys
+import MySQLdb
 
 API_KEY="AIzaSyBOTaWUvzwtWHfEpZ8zH8d-31aKZn6k4Po"
 
-messageTitle = sys.argv[1]
-messageBody = sys.argv[2]
+db = MySQLdb.connect(host="146.185.176.134",    # your host, usually localhost
+                     user="root",               # your username
+                     passwd="root",             # your password
+                     db="Sensoren")             # name of the data base
 
-data={
-    "to" : "/topics/my_little_topic",
-    "notification" : {
-        "body" : messageBody,
-        "title" : messageTitle,
-        "icon" : "ic_cloud_white_48dp"
+cur = db.cursor()
+
+sql_cmd = "SELECT t1.plantid, t1.moisture, t1.timestamp, t3.plantNaam \
+            FROM Planten t1 JOIN (SELECT plantNaam, plantid FROM PlantNamen) as t3 \
+            ON t1.plantid = t3.plantid \
+            JOIN (SELECT plantid, MAX(timestamp) timestamp \
+            FROM Planten GROUP BY plantid) as t2 ON t1.plantid = t2.plantid \
+            AND t1.timestamp = t2.timestamp ORDER BY t1.plantid"
+cur.execute(sql_cmd)
+results = cur.fetchall()
+
+def sendBericht(messageTitle, messageBody):
+    data={
+        "to" : "/topics/my_little_topic",
+        "notification" : {
+            "body" : messageBody,
+            "title" : messageTitle,
+            "icon" : "ic_cloud_white_48dp"
+        }
     }
-}
 
-dataAsJSON = json.dumps(data)
+    dataAsJSON = json.dumps(data)
 
-request = Request(
-    "https://gcm-http.googleapis.com/gcm/send",
-    dataAsJSON,
-    { "Authorization" : "key="+API_KEY,
-      "Content-type" : "application/json"
-    }
-)
+    request = Request(
+        "https://gcm-http.googleapis.com/gcm/send",
+        dataAsJSON,
+        { "Authorization" : "key="+API_KEY,
+          "Content-type" : "application/json"
+        }
+    )
 
-print urlopen(request).read()
+    print urlopen(request).read()
+
+for row in results:
+    plantid = row[0]
+    moisture = row[1]
+    #print "id: %s" % (plantid) + " moisture: %s" % (moisture)
+    if moisture < '35':
+        sendBericht("Plant %s" % (plantid) + " heeft uw aandacht nodig", "Geef het water anders gebeurt dit automatisch")
